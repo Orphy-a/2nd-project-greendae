ecca6b411f1578ed271c73d8160abb3af03ceb41
id3ntity99 <jas06113@gmail.com>
2025-03-31 18:55:33 +0900

    implementing controller for article

--- GreenUniv/src/main/java/com/greenuniv/greenuniv/controller/article/ArticleController.java
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/controller/article/ArticleController.java
@@ -1,8 +1,26 @@
 package com.greenuniv.greenuniv.controller.article;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
+@RequestMapping("/article")
 public class ArticleController {
 
+  @GetMapping("") // == /article
+  public String list(@RequestParam String category,
+      @RequestParam(required = false, defaultValue = "1") int page,
+      @RequestParam(required = false, defaultValue = "10") int offset, Model model) {
+
+    String templatePath = String.format("/community/%s", category);
+    log.info("Incoming request for /article?{}&{}&{}& has been detected", category, page, offset);
+    log.info("Retrieving page for {}", templatePath);
+
+    return templatePath;
+  }
 }
