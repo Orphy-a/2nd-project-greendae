77fc70e9d9ef2a5275e08a539a8b3e9fe3bd1b43
Orphy-a <jaeyoung0560@gmail.com>
2025-04-04 09:57:06 +0900

    회원가입 수정

--- GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/user/UserEntity.java
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/user/UserEntity.java
@@ -2,20 +2,12 @@ package com.greenuniv.greenuniv.entity.user;
 
 import com.greenuniv.greenuniv.dto.user.UserDTO;
 import com.greenuniv.greenuniv.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Getter
@@ -26,99 +18,98 @@ import org.hibernate.annotations.CreationTimestamp;
 @NoArgsConstructor
 public class UserEntity implements BaseEntity {
 
-  //TODO: Impl this
-  @Id
-  private String id;
+    //TODO: Impl this
+    @Id
+    private String id;
 
-  @Column(name = "password")
-  private String password;
+    @Column(name = "password")
+    private String password;
 
-  @Column(name = "name")
-  private String name;
+    @Column(name = "name")
+    private String name;
 
-  private String eng_Name;
-  private String gender;
-  private String nationality;
+    private String eng_Name;
+    private String gender;
+    private String nationality;
 
-  @Column(name = "social_number")
-  private String socialNumber;
+    @Column(name = "social_number")
+    private String socialNumber;
 
-  @Column(name = "email")
-  private String email;
+    @Column(name = "email")
+    private String email;
 
-  @Column(name = "contact")
-  private String contact;
+    @Column(name = "contact")
+    private String contact;
 
-  @Column(name = "zip")
-  private String zip;
+    @Column(name = "zip")
+    private String zip;
 
-  @Column(name = "address")
-  private String address;
+    @Column(name = "address")
+    private String address;
 
-  @Column(name = "address_detail")
-  private String address_detail;
+    @Column(name = "address_detail")
+    private String address_detail;
 
-  // string -> enum 타입 수정
-  @Enumerated(EnumType.STRING)
-  private UserEntity.Role role;
+    // string -> enum 타입 수정
+    @Enumerated(EnumType.STRING)
+    private UserEntity.Role role;
 
-  @Column(name = "agreed_terms")
-  private boolean agreedTerms;
+    @Column(name = "agreed_terms")
+    private boolean agreedTerms;
 
-  @Column(name = "register_date")
-  @CreationTimestamp
-  private LocalDateTime registerDate;
+    @Column(name = "register_date")
+    @CreationTimestamp
+    private LocalDateTime registerDate;
 
-  @Column(name = "leave_date")
-  private LocalDateTime leaveDate;
+    @Column(name = "leave_date")
+    private LocalDateTime leaveDate;
 
-
-  public enum Role {
-    student, professor, admin, general
-  }
-
-
-  @Override
-  public UserDTO toDTO() {
-    return UserDTO.builder()
-        .id(id)
-        .password(password)
-        .name(name)
-        .engName(eng_Name)
-        .gender(gender)
-        .nationality(nationality)
-        .socialNumber(socialNumber)
-        .email(email)
-        .contact(contact)
-        .zip(zip)
-        .address(address)
-        .addressDetail(address_detail)
-        .role(role)
-        .agreedTerms(agreedTerms)
-        .registerDate(registerDate)
-        .leaveDate(leaveDate)
-        .build();
-  }
-
-  public static class UserEntityBuilder {
-
-    public UserEntity build() throws IllegalArgumentException {
-
-      boolean isGenderLegal = Arrays.asList(UserDTO.GENDERS).contains(gender);
-      boolean isRoleLegal = Arrays.asList(UserDTO.ROLES).contains(role);
-
-      if (!isGenderLegal) {
-        String message = String.format("유효하지 않은 성별([%s]): %s", Arrays.toString(UserDTO.GENDERS),
-            gender);
-        throw new IllegalArgumentException(message);
-      } else if (!isRoleLegal) {
-        String message = String.format("유효하지 않은 사용자 유형([%s]): %s", Arrays.toString(UserDTO.ROLES),
-            role);
-        throw new IllegalArgumentException(message);
-      }
-      return new UserEntity(id, password, name, eng_Name, gender, nationality, socialNumber, email,
-          contact, zip, address, address_detail, role, agreedTerms, registerDate, leaveDate);
+    @Override
+    public UserDTO toDTO() {
+        return UserDTO.builder()
+                .id(id)
+                .password(password)
+                .name(name)
+                .engName(eng_Name)
+                .gender(gender)
+                .nationality(nationality)
+                .socialNumber(socialNumber)
+                .email(email)
+                .contact(contact)
+                .zip(zip)
+                .address(address)
+                .address_detail(address_detail)
+                .role(role)
+                .agreedTerms(agreedTerms)
+                .registerDate(registerDate)
+                .leaveDate(leaveDate)
+                .build();
     }
 
-  }
+
+    public enum Role {
+        student, professor, admin, general
+    }
+
+    public static class UserEntityBuilder {
+
+        public UserEntity build() throws IllegalArgumentException {
+
+            boolean isGenderLegal = Arrays.asList(UserDTO.GENDERS).contains(gender);
+            boolean isRoleLegal = Arrays.asList(UserDTO.ROLES).contains(role);
+
+            if (!isGenderLegal) {
+                String message = String.format("유효하지 않은 성별([%s]): %s", Arrays.toString(UserDTO.GENDERS),
+                        gender);
+                throw new IllegalArgumentException(message);
+            } else if (!isRoleLegal) {
+                String message = String.format("유효하지 않은 사용자 유형([%s]): %s", Arrays.toString(UserDTO.ROLES),
+                        role);
+                throw new IllegalArgumentException(message);
+            }
+            return new UserEntity(id, password, name, eng_Name, gender, nationality, socialNumber, email,
+                    contact, zip, address, address_detail, role, agreedTerms, registerDate, leaveDate);
+        }
+
+    }
 }
